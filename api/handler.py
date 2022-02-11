b3b75decbb01bef8e827742fc63790d5db5bda26
hfpmatheus <matheushffp@gmail.com>
2022-02-11 16:30:29 -0300

    ''

---
+++ api/handler.py
@@ -0,0 +1,44 @@
+import pickle
+import pandas as pd
+from flask           import Flask, request, Response
+from rossmann.Rossmann import Rossmann
+
+# loading model
+model = pickle.load( open( '/Users/mathe/Repos_ComunidadeDS/rossmann_store_sales/rosmann_model.pkl' , 'rb' ) )
+
+app = Flask( __name__ )
+
+@app.route( '/rossmann/predict', methods=['POST'] )
+
+def rossmann_predict():
+    test_json = request.get_json()
+    
+    if test_json: # there is data
+        if isinstance( test_json, dict ): # unique example
+            test_raw = pd.DataFrame( test_json, index=[0] )
+            
+        else: # multiple example
+            test_raw = pd.DataFrame( test_json, columns = test_json[0].keys() )
+            
+        # Instantiate rossmann class
+        pipeline = Rossmann()
+        
+        # data cleaning
+        df1 = pipeline.data_cleaning( test_raw )
+        
+        # feature engineering
+        df2 = pipeline.feature_engineering( df1 )
+        
+        # data transformation
+        df3 = pipeline.data_transformation( df2 )
+        
+        # get prediction
+        df_response = pipeline.get_prediction( model, test_raw, df3 )
+        
+        return df_response
+    
+    else:
+        return Response( '{}', status=200, mimetype='application/json' )
+
+if __name__ == '__main__':
+    app.run( '0.0.0.0' )
